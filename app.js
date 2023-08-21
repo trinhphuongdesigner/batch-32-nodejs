083a90ab922cb44455022f9bd68ce0770c3b1bb0
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-08-21 19:40:16 +0700

    employee

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const productsRouter = require('./routes/product/router');
 const categoriesRouter = require('./routes/category/router');
 const suppliersRouter = require('./routes/supplier/router');
 const customersRouter = require('./routes/customer/router');
+const employeesRouter = require('./routes/employee/router');
 
 const app = express();
 
@@ -32,6 +33,7 @@ app.use('/products', productsRouter);
 app.use('/categories', categoriesRouter);
 app.use('/suppliers', suppliersRouter);
 app.use('/customers', customersRouter);
+app.use('/employees', employeesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Category = require('./category');
 const Supplier = require('./supplier');
 const Customer = require('./customer');
+const Employee = require('./employee');
 
 module.exports = {
   Category,
   Supplier, 
   Customer,
+  Employee,
 };
