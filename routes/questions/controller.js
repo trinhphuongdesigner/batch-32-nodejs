cdacd9b59c23524447bdbcf4335a52793847186e
@phuongtd.wiicamp <phuongtd@wiicamp.com>
2023-08-28 20:02:52 +0700

    query

--- routes/questions/controller.js
+++ routes/questions/controller.js
@@ -30,4 +30,72 @@ module.exports = {
       return res.status(500).json({ code: 500, error: err });
     }
   },
+
+  question1a: async (req, res, next) => {
+    try {
+      const { discount } = req.query;
+      const conditionFind = {};
+
+      if (discount) conditionFind.discount = { $gte: discount };
+
+      let results = await Product.find(conditionFind).populate("category").populate("supplier");
+      let total = await Product.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
+
+  question1b: async (req, res, next) => {
+    try {
+      const { discount, type } = req.query;
+      const conditionFind = {};
+
+      if (discount) {
+        switch (Number(type)) {
+          case 0:
+            conditionFind.discount = { $eq: discount };
+            break;
+
+          case 1:
+            conditionFind.discount = { $lt: discount };
+            break;
+
+          case 2:
+            conditionFind.discount = { $lte: discount };
+            break;
+
+          case 3:
+            conditionFind.discount = { $gt: discount };
+            break;
+
+          case 4:
+            conditionFind.discount = { $gte: discount };
+            break;
+
+          default:
+            conditionFind.discount = { $eq: discount };
+            break;
+        }
+      }
+
+      let results = await Product.find(conditionFind).populate("category").populate("supplier");
+      let total = await Product.countDocuments();
+
+      return res.send({
+        code: 200,
+        total,
+        totalResult: results.length,
+        payload: results,
+      });
+    } catch (err) {
+      return res.status(500).json({ code: 500, error: err });
+    }
+  },
 };
--- routes/questions/router.js
+++ routes/questions/router.js
@@ -8,8 +8,8 @@ const {
 } = require('./validations');
 const {
   question1,
-  // question1a,
-  // question1b,
+  question1a,
+  question1b,
   // question2a,
   // question2b,
   // question3,
@@ -43,8 +43,8 @@ const {
 } = require('./controller');
 
 router.get('/1', question1);
-// router.get('/1a', validateSchema(q1), question1a);
-// router.get('/1b', question1b);
+router.get('/1a', question1a);
+router.get('/1b', question1b);
 // router.get('/2a', question2a);
 // router.get('/2b', question2b);
 // router.get('/3', question3);
